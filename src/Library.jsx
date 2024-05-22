51732679d2874a19369664833c526806f61b2416
mohammadjamshidi8 <mohamadjamshidiiii8@gmail.com>
2024-05-22 20:02:38 +0330

    update files

--- src/Library.jsx
+++ src/Library.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import LibrarySongs from './LibrarySongs'
+import React from "react";
+import LibrarySongs from "./LibrarySongs";
 
-export default function Library({songs , setCurrentSong}) {
+export default function Library({ songs, setCurrentSong }) {
   return (
-    <div className='relative'>
-        <div className='shadow-xl w-[400px] h-full fixed left-0 top-0 flex flex-col gap-y-4 px-5 pt-5 '>
-            {songs.map(song => <LibrarySongs allSongs={songs} song={song} setCurrentSong={setCurrentSong} />)}
-        </div>
+    <div className="relative">
+      <div className="shadow-xl w-[400px] h-full fixed left-0 top-0 flex flex-col gap-y-4 px-5 pt-5 ">
+        {songs.map((song) => (
+          <LibrarySongs
+            key={song.id}
+            allSongs={songs}
+            song={song}
+            setCurrentSong={setCurrentSong}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
